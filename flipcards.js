0a896d640a183edcfafbd6514eaef92897675b3a
xen1ny <stasii.nasinczuk7@gmail.com>
2024-10-13 19:44:54 +0200

    Add files via upload

---
+++ flipcards.js
@@ -0,0 +1,8 @@
+function flipCard(card) {
+    card.classList.toggle('flip');
+  }
+  
+function toggleMenu() {
+    const menu = document.querySelector('.nav-menu');
+    menu.classList.toggle('show'); 
+}
